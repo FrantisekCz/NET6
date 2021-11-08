93f5a778696fa37e17cc23bad9259ec02aac3fc7
František Pangrác <frantisek.pangrac@email.cz>
2021-11-08 16:35:33 +0100

    UrcitPlnoletostUzivatele

--- 01 HelloWorld/01 Hello World/Hello World/Program.cs
+++ 01 HelloWorld/01 Hello World/Hello World/Program.cs
@@ -8,6 +8,36 @@ namespace Hello_World
         static void Main(string[] args)
         {
 
+            UrcitPlnoletostUzivatele();
+
+        }
+
+
+        static void UrcitPlnoletostUzivatele()
+        {
+            Console.WriteLine("Zadej datum narození (YYYY-MM-DD):");
+            DateTime dDatumNarozeni = DateTime.Parse(Console.ReadLine());
+            TimeSpan tsRozdilNarozeniNow = DateTime.Now - dDatumNarozeni;
+            double dbPocetDnuRozdilu = tsRozdilNarozeniNow.TotalDays;
+            // urcite pod 18
+            if(dbPocetDnuRozdilu < (18*365 + 3) )
+            {
+                Console.WriteLine("Plnoletost nedosažena");
+            }
+            else if (dbPocetDnuRozdilu > (18 * 365 + 6))
+            {
+                Console.WriteLine("SUPER, plnoletost dosažena.");
+            }
+            else
+            {
+                Console.WriteLine("Nějak si nejsem jistý.");
+            }
+
+        }
+
+
+        static void PraceSDatemCasem()
+        {
             // PRÁCE S DATEM A ČASEM
             // vytváří se nová instance objektu. Podle počtu parametrů se to chová.
             DateTime dDatum = new DateTime(2021, 11, 8);
@@ -17,13 +47,11 @@ namespace Hello_World
             DateTime dtNyni = DateTime.Now;
 
             // instanční metody
-            DateTime dDatumZa14Dni  = dDatum.AddDays(14);
-            int iDatumRok           = dDatum.Year;
-            string sKratkeDatum     = dDatum.ToShortDateString();
+            DateTime dDatumZa14Dni = dDatum.AddDays(14);
+            int iDatumRok = dDatum.Year;
+            string sKratkeDatum = dDatum.ToShortDateString();
 
             TimeSpan dtRozdilDateTime = dtNyni - dtDatumCas;
-
         }
-
     }
 }
