149ca12c8ff2f842021f9e946dc3268f8c7a506d
František Pangrác <frantisek.pangrac@email.cz>
2021-11-08 11:31:19 +0100

    Input/Output

--- 01 HelloWorld/01 Hello World/Hello World/Program.cs
+++ 01 HelloWorld/01 Hello World/Hello World/Program.cs
@@ -6,7 +6,14 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+ 
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Jak se jmenuješ?");
+            // necháme si předat od uživatele jméno
+            string sName = Console.ReadLine();
+
+            string sPozdrav = "Ahoj, ty jsi " + sName;
+            Console.WriteLine(sPozdrav);
         }
     }
 }
