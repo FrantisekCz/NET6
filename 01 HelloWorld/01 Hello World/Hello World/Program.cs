02791ff3d52fd5c489316dc32201cf649d663eef
František Pangrác <frantisek.pangrac@email.cz>
2021-11-08 15:59:15 +0100

    fce ZjistiZapisJmenoVek

--- 01 HelloWorld/01 Hello World/Hello World/Program.cs
+++ 01 HelloWorld/01 Hello World/Hello World/Program.cs
@@ -7,144 +7,25 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+            while( ZjistiZapisJmenoVek() )
+            {
+                // vykonavej dokud nevratim false
+            };
 
-            /*
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Jak se jmenuješ?");
-            // necháme si předat od uživatele jméno
+
+        }
+
+        static bool ZjistiZapisJmenoVek()
+        {
+            Console.WriteLine(Environment.NewLine + "Zadejte své jméno (x pro konec):");
             string sJmeno = Console.ReadLine();
-            Pozdravit(sJmeno);
-            */
+            if (sJmeno == "x") return false;
+            Console.WriteLine("Zadejte svůj věk:");
+            string sVek = Console.ReadLine();
 
-            // kalkulacka
-            /*
-            Console.WriteLine("Zadej mi nějaké celé číslo:");
-            int iVstup = int.Parse(Console.ReadLine());
-            Console.WriteLine("Výsledek příkladu (" + iVstup + " x 2 + 10) je " + Vypocet(iVstup) + ".\n");
-            */
-
-            // prevod teplot
-            //PrevedTiskniFtoC();
-
-
-            // CYKLUS WHILE
-            /*
-            int iCounter = 0;
-            while( iCounter < 10)
-            {
-                Console.WriteLine(iCounter++);
-//                iCounter++;
-            }
-            */
-
+            File.AppendAllText("jmena.txt", ($"{sJmeno};{sVek}" + Environment.NewLine));
+            return true;
             
-            string sVstup = "";
-            while(sVstup != "x")
-            {
-                sVstup = PrevadejFtoCVyjimky();
-            }
-
-        }
-
-
-
-
-
-        static string PrevadejFtoCVyjimky()
-        {
-            double dbTeplotaF = 0;
-            Console.WriteLine("Zadejte teplotu v °F nebo zadejte \"x\" pro ukončení:");
-            string sHodnota = Console.ReadLine();
-
-            try
-            {
-                dbTeplotaF = double.Parse(sHodnota);
-                Console.WriteLine($"Teplota {dbTeplotaF}°F odpovídá {PrevodFtoC(dbTeplotaF)}°C.\n");
-            }
-            catch(Exception exVyjimka)
-            {
-                //NewLine zajistuje, ze to bude na vsech systemech spravne
-                File.AppendAllText( "errorlog.txt", exVyjimka.Message + Environment.NewLine);
-            }
-
-            return sHodnota;
-
-        }
-
-
-
-
-        static string PrevadejFtoCneboSkonci()
-        {
-            double dbTeplotaF = 0;
-            Console.WriteLine("Zadejte teplotu v °F nebo zadejte \"x\" pro ukončení:");
-            string sHodnota = Console.ReadLine();
-
-            // pokud je x, pak vraci hodnotu a vypadne
-            if (sHodnota == "x")
-            {
-                return sHodnota;
-            }
-            else
-            {
-                // toto probehne jen pokud neni "x"
-                dbTeplotaF = double.Parse(sHodnota);
-                Console.WriteLine($"Teplota {dbTeplotaF}°F odpovídá {PrevodFtoC(dbTeplotaF)}°C.\n");
-                return sHodnota;
-            }
-        }
-
-
-        /// <summary>
-        /// Vypočte a vypíše teplotu v °C z teploty v °F
-        /// </summary>
-        static void PrevedTiskniFtoC()
-        {
-            Console.WriteLine("Zadej teplotu ve F°:");
-            double dbTeplotaF = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Teplota {dbTeplotaF}°F odpovídá {PrevodFtoC(dbTeplotaF)}°C.\n");
-        }
-
-        static int Vypocet( int iCisloVstup )
-        {
-            return (iCisloVstup * 2) + 10;
-        }
-
-
-
-        static double PrevodFtoC( double dbTeplotaF )
-        {
-            return (dbTeplotaF - 32) / 1.8;
-        }
-
-
-        static void Pozdravit(string sJmeno)
-        {
-
-            // jméno vyskloňujeme
-            string sOsloveni = "cizinče jménem " + sJmeno;
-            if (sJmeno == "František")
-            {
-                sOsloveni = "Františku";
-            }
-            else if (sJmeno == "Jarda")
-            {
-                sOsloveni = "Jarouši";
-            }
-            else if (sJmeno == "Pavel")
-            {
-                sOsloveni = "Pavlíku";
-            }
-            else sOsloveni = sOsloveni + ", tvoje jméno neumím skloňovat :-(";
-            // lze napsat i takto: sOsloveni += pridany_text
-
-
-            
-
-            // pěkně pozdravíme
-            // string sPozdrav = "Ahoj, ty jsi " + sName;
-            string sPozdravOsloveni = "Nazdárek " + sOsloveni;
-            Console.WriteLine(sPozdravOsloveni);
         }
     }
 }
