bbd231dd09c0e28616e5731757940fc1f84c2af2
František Pangrác <frantisek.pangrac@email.cz>
2021-11-08 16:21:32 +0100

    práce s datem a časem

--- 01 HelloWorld/01 Hello World/Hello World/Program.cs
+++ 01 HelloWorld/01 Hello World/Hello World/Program.cs
@@ -8,16 +8,21 @@ namespace Hello_World
         static void Main(string[] args)
         {
 
-            while (true)
-            {
-                Console.WriteLine(Environment.NewLine + "Zadejte své jméno (x pro konec):");
-                string sJmeno = Console.ReadLine();
-                if (sJmeno == "x") return;  // nasilne ukonci program; = halt
-                Console.WriteLine("Zadejte svůj věk:");
-                string sVek = Console.ReadLine();
+            // PRÁCE S DATEM A ČASEM
+            // vytváří se nová instance objektu. Podle počtu parametrů se to chová.
+            DateTime dDatum = new DateTime(2021, 11, 8);
+            DateTime dtDatumCas = new DateTime(2021, 11, 8, 16, 11, 00);
 
-                File.AppendAllText("jmena.txt", ($"{sJmeno};{sVek}" + Environment.NewLine));
-            }
+            //statické metody
+            DateTime dtNyni = DateTime.Now;
+
+            // instanční metody
+            DateTime dDatumZa14Dni  = dDatum.AddDays(14);
+            int iDatumRok           = dDatum.Year;
+            string sKratkeDatum     = dDatum.ToShortDateString();
+
+            TimeSpan dtRozdilDateTime = dtNyni - dtDatumCas;
+
         }
 
     }
