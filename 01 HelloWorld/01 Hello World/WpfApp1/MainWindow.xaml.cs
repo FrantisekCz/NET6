70eb139189d9038e75b052353db29649faffe92a
František Pangrác <frantisek.pangrac@email.cz>
2021-11-09 16:32:06 +0100

    První okénková klikací superaplikace!

--- 01 HelloWorld/01 Hello World/WpfApp1/MainWindow.xaml.cs
+++ 01 HelloWorld/01 Hello World/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txbInfo.Text = "HA, klikl jsi na tlačítko!";
+            btnKlikni.Content = "Paráda, viď?";
+        }
     }
 }
